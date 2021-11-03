022affcae6244c7bbb307264b7e74c9c60019832
Jiyoung Kim <jyk@mhand.net>
2021-11-03 21:01:32 +0900

    feat: Add TabBox/index.ts

---
+++ src/components/TabBox/index.tsx
@@ -0,0 +1,2 @@
+export { default as TabBox } from "./TabBox";
+export type { TabBoxProps } from "./types";
--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ export * from "./components/Spinner";
 export * from "./components/Skeleton";
 export * from "./components/Toggle";
 export * from "./components/Table";
+export * from "./components/TabBox";
 
 // Hooks
 export * from "./hooks";
