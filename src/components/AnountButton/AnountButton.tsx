febf00422c6289c57afa0c3ca4447fb8c9bf2e10
GeonHyeok-Lee <geonhyeoklee.kr@gmail.com>
2021-11-04 14:18:27 +0900

    fix: Change AnountButton props

--- src/components/AnountButton/AnountButton.tsx
+++ src/components/AnountButton/AnountButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AnountButtonProps } from "./types";
 import { StyledAnountButton } from "./StyledAnountButton";
 
-const AnountButton: React.FC<AnountButtonProps> = ({ children, ...props }) => {
-  return <StyledAnountButton {...props}>{children}</StyledAnountButton>;
+const AnountButton: React.FC<AnountButtonProps> = ({ onClick, children, ...props }) => {
+  return <StyledAnountButton onClick={onClick} {...props}>{children}</StyledAnountButton>;
 };
 export default AnountButton;
