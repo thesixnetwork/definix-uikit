37190295d4a05a01d11670577e3373e7acd4c088
lotus1128 <50655406+lotus1128@users.noreply.github.com>
2021-11-04 17:41:43 +0900

    feat: Add dropdown attr

--- src/components/Dropdown/DropdownItem.tsx
+++ src/components/Dropdown/DropdownItem.tsx
@@ -2,9 +2,9 @@ import { hexToRGB } from "@/style/mixin";
 import { textStyle, TextStyles } from "@/theme";
 import React from "react";
 import styled, { css } from "styled-components";
-import { DropdownItemProps } from "./types";
+import { DropdownItemProps, Scale, scales } from "./types";
 
-const Item = styled.div<{ isActive: boolean }>`
+const Item = styled.div<{ isActive: boolean, scale: Scale, isDivide?: boolean }>`
   cursor: pointer;
   padding: 0 16px;
   min-width: 100%;
@@ -12,15 +12,16 @@ const Item = styled.div<{ isActive: boolean }>`
   display: flex;
   align-items: center;
   color: ${({ theme }) => theme.colors.deepgrey};
-  ${({ isActive }) => css(textStyle[isActive ? TextStyles.R_14M : TextStyles.R_14R])}
+  ${({ isActive, scale }) => css(textStyle[isActive ? scales[scale].activeTextStyle : scales[scale].textStyle])}
+  ${({ isDivide, theme }) => isDivide && `border-top: 1px solid ${theme.colors.lightgrey};`}
   white-space: nowrap;
 
   background-color: ${({ isActive, theme }) => (isActive ? hexToRGB(theme.colors.pale, 0.2) : theme.colors.white)};
 `;
 
-const DropdownItem: React.FC<DropdownItemProps> = ({ children, isActive = false, ...props }) => {
+const DropdownItem: React.FC<DropdownItemProps> = ({ children, isActive = false, scale = 'md', ...props }) => {
   return (
-    <Item isActive={isActive} {...props}>
+    <Item isActive={isActive} scale={scale} {...props}>
       {children}
     </Item>
   );
