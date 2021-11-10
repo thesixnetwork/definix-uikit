56cce08ab95f38064ef1cbf22697ef39142b3d78
Jiyoung Kim <jyk@mhand.net>
2021-11-10 20:15:53 +0900

    fix: Add export ButtonGroup

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,6 @@
 export { default as Button } from "./Button";
 export { default as IconButton } from "./IconButton";
 export { default as DropdownButton } from "./DropdownButton";
+export { default as ButtonGroup } from "./ButtonGroup";
 export { ButtonScales, ButtonVariants } from "./types";
-export type { ButtonProps, BaseButtonProps } from "./types";
+export type { ButtonProps, BaseButtonProps, ButtonGroupProps } from "./types";
