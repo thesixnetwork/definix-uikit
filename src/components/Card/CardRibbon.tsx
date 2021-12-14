872ecc942666fed2307a8463c292bfcb029f22a1
young <kimsy89@mhand.net>
2021-12-14 15:29:17 +0900

    chore: Update cardRibbon style

--- src/components/Card/CardRibbon.tsx
+++ src/components/Card/CardRibbon.tsx
@@ -11,16 +11,20 @@ interface StyledCardRibbonProps extends CardRibbonProps {
 
 const StyledCardRibbon = styled.div<Partial<StyledCardRibbonProps>>`
   background-color: ${({ variantColor = "yellow", theme }) => theme.colors[variantColor]};
-  padding: ${pxToRem(5)} ${pxToRem(spacing.S_20)};
   position: absolute;
   left: unset;
-  top: 17px;
+  top: 16px;
+  padding: 5px ${({ theme }) => theme.spacing.S_20}px;
   border-top-right-radius: ${pxToRem(spacing.S_8)};
   border-bottom-right-radius: ${pxToRem(spacing.S_8)};
   z-index: 2;
   ${textStyle.R_12B}
   color: white;
   ${({ upperCase }) => upperCase && "text-transform: uppercase;"};
+  ${({ theme }) => theme.mediaQueries.mobileXl} {
+    top: 12px;
+    padding: 3px ${({ theme }) => theme.spacing.S_12}px;
+  }
 `;
 
 const CardRibbon: React.FC<CardRibbonProps> = ({ variantColor = "yellow", text, upperCase }) => {
