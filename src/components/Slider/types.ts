e72500b2309b060cd27d7f414ca45755e74a20d9
GeonHyeok-Lee <geonhyeoklee.kr@gmail.com>
2021-12-16 17:09:10 +0900

    fix: Change noti style

--- src/components/Slider/types.ts
+++
@@ -1,9 +0,0 @@
-import { BoxProps } from "../Box/types";
-
-export interface SliderProps extends BoxProps {
-  min: number;
-  max: number;
-  value: number;
-  onValueChanged: (newValue: number) => void;
-  valueLabel?: string;
-}
