ec010e3b1d5d161cc15f2019bf5f414d07d2def5
Jinyoung Ahn <yj6210ag@gmail.com>
2021-12-16 17:13:50 +0900

    chore: Add LongTermStake Image

--- src/components/Finix/index.tsx
+++ src/components/Finix/index.tsx
@@ -33,3 +33,7 @@ export { default as ImgError3x } from "./images/img_error@3x.png";
 export { default as ImgMyinvestmentDefault1x } from "./images/img_myinvestment_default.png";
 export { default as ImgMyinvestmentDefault2x } from "./images/img_myinvestment_default@2x.png";
 export { default as ImgMyinvestmentDefault3x } from "./images/img_myinvestment_default@3x.png";
+
+export { default as ImgLongterm1x } from "./images/img-longterm.png";
+export { default as ImgLongterm2x } from "./images/img-longterm@2x.png";
+export { default as ImgLongterm3x } from "./images/img-longterm@3x.png";
