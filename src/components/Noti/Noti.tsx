3215d7201544a6ef7156f2b4bf0220220e2d4d03
GeonHyeok-Lee <geonhyeoklee.kr@gmail.com>
2021-12-25 11:40:55 +0900

    fix: Change Noti Icon

--- src/components/Noti/Noti.tsx
+++ src/components/Noti/Noti.tsx
@@ -4,6 +4,7 @@ import { Text } from "../Text";
 import styled from "styled-components";
 import { space, SpaceProps } from "styled-system";
 import { ColorStyles } from "../../theme";
+import { AlertIcon } from "../Icon";
 
 export enum NotiType {
   ALERT = "alert",
@@ -20,34 +21,13 @@ const StyledNoti = styled.div`
   ${space}
 `;
 
-const StyledIcon = styled.div`
-  width: 14px;
-  height: 14px;
-  min-width: 14px;
-  min-height: 14px;
-  margin-right: 5px;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-size: 10px;
-  &.alert {
-    background-color: ${({ theme }) => theme.colors.red};
-    color: ${({ theme }) => theme.colors.white};
-  }
-  &.guide {
-    border: 1.2px solid ${({ theme }) => theme.colors.mediumgrey};
-    color: ${({ theme }) => theme.colors.mediumgrey};
-  }
-`;
-
 const Noti: React.FC<NotiProps> = ({ type, children, ...props }) => {
   return (
     <StyledNoti {...props}>
       <Flex alignItems="center">
-        <StyledIcon className={type}>!</StyledIcon>
+        <AlertIcon />
         <Text
+          ml="4px"
           textStyle={type === NotiType.ALERT ? "R_14R" : "R_12R"}
           color={type === NotiType.ALERT ? ColorStyles.RED : ColorStyles.MEDIUMGREY}
         >
