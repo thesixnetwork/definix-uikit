67139af3c2c821f13a9f11ee8fd66d22eaeb404b
lotus1128 <50655406+lotus1128@users.noreply.github.com>
2022-01-24 17:36:49 +0900

    chore: Edit lint

--- src/widgets/Menu/components/FinixPrice.tsx
+++ src/widgets/Menu/components/FinixPrice.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import { Coin, ColorStyles, Flex, Text } from "src";
 
 interface IProps {
   price?: string | number;
 }
 
-const FinixPrice: React.FC<IProps> = ({price = '0.0000'}) => {
+const FinixPrice: React.FC<IProps> = ({ price = "0.0000" }) => {
   return (
     <Flex alignItems="center">
       <Coin symbol="FINIX" size="18px" />
-      <Text ml="6px" textStyle="R_14B" color={ColorStyles.BLACK}>${price}</Text>
+      <Text ml="6px" textStyle="R_14B" color={ColorStyles.BLACK}>
+        ${price}
+      </Text>
     </Flex>
-  )
-}
+  );
+};
 
 export default React.memo(FinixPrice);
