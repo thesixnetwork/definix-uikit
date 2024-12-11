49d6aaa0d8d477302043e68c7dffaf21c42f3a18
toonp <patchara.a@six.network>
2024-12-11 14:58:23 +0700

    fix: Update version

--- dist/widgets/WalletModal/types.d.ts
+++ dist/widgets/WalletModal/types.d.ts
@@ -1,19 +1,18 @@
 import { FC } from "react";
 import { SvgProps } from "../../components/Svg/types";
 export declare enum ConnectorNames {
-  // Kaikas = "kaikas",
-  Kaia = "kaia",
-  Dcent = "dcent",
-  Klip = "klip",
-  TokenPocket = "tokenpocket",
-  WalletConnect = "walletconnect",
-  BSC = "bsc",
+    Kaia = "kaia",
+    Dcent = "dcent",
+    Klip = "klip",
+    TokenPocket = "tokenpocket",
+    WalletConnect = "walletconnect",
+    BSC = "bsc"
 }
 export declare type Login = (connectorId: ConnectorNames) => void;
 export interface Config {
-  title: string;
-  icon: FC<SvgProps>;
-  connectorId: ConnectorNames;
-  width?: number;
-  height?: number;
+    title: string;
+    icon: FC<SvgProps>;
+    connectorId: ConnectorNames;
+    width?: number;
+    height?: number;
 }
